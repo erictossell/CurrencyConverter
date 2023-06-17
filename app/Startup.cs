49b6d7c46e14c8407cceeea8fb0ecb0f0fd632e9
David Wagner <david.karl.wagner@live.com>
2023-06-17 13:00:36 -0400

    restrutctured as per docker doc

---
+++ app/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CurrencyConverter
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.SendFileAsync("wwwroot/index.html");
+                });
+            });
+        }
+    }
+}
---
+++ app/controllers/ConvertController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using CurrencyConverter.Helpers;
+
+
+namespace CurrencyConverter.Controllers
+{
+    [ApiController]
+    [Route("/api/convert")]
+    public class ConvertController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(decimal amount, string from, string to)
+        {
+            // Replace this with your existing C# logic for currency conversion
+            string convertedAmount = CurrencyConverter.Convert(amount, from, to);
+            if (convertedAmount != "")
+            {
+                return Ok(new { success = true, result = convertedAmount });
+            }
+            else
+            {
+                return BadRequest(new { success = false, message = "Invalid conversion." });
+            }
+        }
+    }
+
+    public static class CurrencyConverter
+    {
+        public static string Convert(decimal amount, string from, string to)
+        {
+            CurrencyAPI api = new CurrencyAPI();
+            var exchangeRate = api.Get($"https://api.freecurrencyapi.com/v1/latest?apikey=x0fISZ5yVuubfp91QA5YJyisbCvxqKkMMpjkOdkc&base_currency={from.ToUpper()}&currencies={to.ToUpper()}");
+            string rate = exchangeRate.ToString();
+            rate = Regex.Split(rate, @"\:")[Regex.Split(rate, @"\:").Length - 1];
+            Match match = Regex.Match(rate, @"[0-9\.]+");
+            Console.WriteLine(match.Success.ToString() ?? "FAIL");
+            Console.WriteLine(match.Value.ToString() ?? "EMPTY");
+            decimal conv = 0;
+            if(match.Success && match.Value != null){
+                decimal.TryParse(match.Value, out conv);
+            }
+            conv = conv * amount;
+
+            string output = api.GetSymbol(to) + conv.ToString();
+            return output;
+        }
+        
+    }
+}
